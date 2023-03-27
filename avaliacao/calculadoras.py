ae6015c10038d91d49bb87ae0272b2a92bf569aa
anabelly <anabellymontibeller@gmail.com>
2023-03-27 15:09:26 -0300

    segundabranch

---
+++ avaliacao/calculadoras.py
@@ -0,0 +1,7 @@
+print('Bem vindo a calculadora Bosch')
+
+print('\nOperações:\n1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação')
+
+op = int(input('\nQual operação você deseja fazer? '))
+valor1 = float(input('Digite o primeiro valor: '))
+valor2 = float(input('Digite o segundo valor: '))
---
+++ feature.py
@@ -0,0 +1,14 @@
+if op == 1:
+    print('\nR:', valor1 + valor2)
+
+elif op == 2:
+    print('\nR:', valor1 - valor2)
+
+elif op == 3:
+    print('\nR:', valor1 / valor2)
+
+elif op == 4:
+    print('\nR:', valor1 * valor2)
+
+else:
+    print('Algo deu errado')
